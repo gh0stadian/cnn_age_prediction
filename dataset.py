e2efbce2e4a5d735e3c928bc38a4224c525ba600
gh0stadian <jan.balucha7@gmail.com>
2022-03-31 02:18:43 +0200

    fix bugs

--- dataset.py
+++ dataset.py
@@ -1,14 +1,17 @@
+import numpy as np
+from PIL import Image
 import os
 import torch
 from torch.utils.data import Dataset
-from torchvision.io import read_image
-import torchvision.transforms.functional as fn
+import torch.nn.functional as F
+import torchvision.transforms
 
 
 class FaceDataset(Dataset):
-    def __init__(self, dataframe, img_root_dir, transform=None):
+    def __init__(self, dataframe, img_root_dir, num_classes, transform=None):
         self.dataframe = dataframe
         self.img_root_dir = img_root_dir
+        self.num_classes = num_classes
         self.transform = transform
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -17,7 +20,7 @@ class FaceDataset(Dataset):
 
     def __getitem__(self, idx):
         img = self.get_img(idx)
-        age = torch.tensor(self.dataframe['age'][idx]).to(self.device),
+        age = F.one_hot(torch.tensor(self.dataframe['age'][idx]), num_classes=self.num_classes).to(dtype=torch.float32)
         name = str(self.dataframe['name'][idx])
 
         if self.transform:
@@ -27,5 +30,6 @@ class FaceDataset(Dataset):
 
     def get_img(self, idx):
         img_path = os.path.join(self.img_root_dir, self.dataframe['full_path'][idx])
-        img = read_image(img_path).to(self.device)
-        return img
+        img = Image.open(img_path).convert('RGB')
+        img = torchvision.transforms.ToTensor()(img)
+        return img.to(dtype=torch.float32)
--- main.py
+++ main.py
@@ -11,21 +11,24 @@ from torchvision import transforms
 from dataset import FaceDataset
 from train import Model
 
+# one image is approx. 440MB
 wandb.init(
     project="test-project",
     entity="jbdb",
     config={
         'epoch': 1,
-        'batch_size': 1,
+        'batch_size': 10,
         'lr': 0.01,
     }
 )
 wandb_logger = WandbLogger(log_model=True)
 
 df = pd.read_csv("cured_imdb.csv", low_memory=False)
+num_classes = df['age'].max()
 
 dataset = FaceDataset(dataframe=df,
                       img_root_dir='imdb_crop/',
+                      num_classes=num_classes,
                       transform=transforms.Compose([transforms.Resize((224, 224))])
                       )
 
@@ -33,17 +36,19 @@ valid_test_length = round(len(dataset) * 0.15)
 lengths = [len(dataset) - 2 * valid_test_length, valid_test_length, valid_test_length]
 train_dataset, valid_dataset, test_dataset = random_split(dataset, lengths)
 
-train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
-valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=wandb.config['batch_size'], shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=wandb.config['batch_size'], shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=wandb.config['batch_size'], shuffle=False)
 
-model = get_pretrained_model()
+model = get_pretrained_model(num_classes=num_classes)
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=wandb.config['lr'])
 
 model = Model(model, criterion, optimizer)
 trainer = pl.Trainer(gpus=1,
                      benchmark=True,
-                     logger=wandb_logger
+                     logger=wandb_logger,
+                     max_epochs=1,
+                     devices=1
                      )
 trainer.fit(model, train_loader, valid_loader)
